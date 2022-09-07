06e8d6d22eb89c6c77f21c5da987aec2ddf26e1f
Paulo Marcos <paulo.marcos.de.assis@gmail.com>
2022-09-07 10:25:58 -0300

    First component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,13 @@
+import NotificationButton from '../src/Components/Notification button'
+
 
 function App() {
-return ( <h1>OSS, Artista</h1>)
+return (
+  <>
+   <h1>OSS, Artista</h1>
+   <NotificationButton/>
+  </>
+)
 }
 
 export default App
---
+++ frontend/src/Components/Notification button/index.tsx
@@ -0,0 +1,13 @@
+import img from  '../../assets/img/notification-icon.svg'
+
+import './Styles.css'
+
+function NotificationButton() {
+    return (
+        <div className="dsmeta-red-btn">
+            <img src={img} alt="Notificar" />
+        </div>
+    )
+}
+
+export default NotificationButton
